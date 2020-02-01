919e09243c9bb26761fa1318031fd1d855e7fd1e
Bruno Gustavo <bruno2011gustavo@hotmail.com>
2020-02-01 17:57:43 -0300

    fix(backend): habilitado cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://app:app2020@mycluster-0dzas.mongodb.net/test?retryWrites=true,', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-app.listen(3000, () => console.log);
 
+
+app.listen(3333, () => console.log);
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
